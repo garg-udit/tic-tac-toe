9848ad4b7bd72a673c2af9cd748c7deab09d66e8
garg-udit <uditgarg540@gmail.com>
2024-01-09 14:34:23 +0530

    update

--- src/Components/TicTacToe/TicTacToe.jsx
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -4,24 +4,28 @@ import circle_icon from '../Assets/circle.png'
 import cross_icon from '../Assets/cross.png'
 
 let data = ["", "", "", "", "", "", "", "", ""]
+let clicks = ["", "", "", "", "", "", "", "", ""]
 
 const TicTacToe = () => {
 
     let [count, setCount] = useState(0);
     let [lock, setLock] = useState(false);
     let titleRef = useRef(null);
+    const [title, setTitle] = useState('Tic Tac Toe');
+
 
     const toggle = (e, num) => {
         if (lock) {
             return 0;
         }
         if (count % 2 === 0) {
-            e.target.innerHTML = `<img src = '${cross_icon}'>`;
+            // e.target.innerHTML = `<img src = '${cross_icon}'>`;/
+            // clicks[num] = ''
             data[num] = 'x';
             setCount(++count);
         }
         else {
-            e.target.innerHTML = `<img src = '${circle_icon}'>`;
+            // e.target.innerHTML = `<img src = '${circle_icon}'>`;
             data[num] = 'o';
             setCount(++count);
         }
@@ -49,37 +53,44 @@ const TicTacToe = () => {
         else if (data[0] === data[4] && data[4] === data[8] && data[8] !== "") {
             wins(data[8]);
         }
-        else if (data[2] === data[4] && data[4] === data[4] && data[6] !== "") {
+        else if (data[2] === data[4] && data[4] === data[6] && data[6] !== "") {
             wins(data[6]);
+        }else if(data.every(d=> d !== "")){
+            setTitle("It's Tie")
         }
     }
     const wins = (winner) => {
         setLock(true);
         if (winner === 'x') {
-            titleRef.current.innerHTML = `Team A Won`
+            // titleRef.current.innerHTML = `Team A Won`
+            setTitle('Team A Won');
         }
         else if (winner === 'o') {
-            titleRef.current.innerHTML = `Team B Won`
+            // titleRef.current.innerHTML = `Team B Won`
+            setTitle('Team B Won');
         }
     }
     return (
         <div className='container'>
-            <h1 className='title' ref={titleRef}> Tic Tac Toe</h1>
+            <h1 className='title' ref={titleRef}> {title}</h1>
             <div className="board">
                 <div className="row">
-                    <div className="boxes" onClick={(e) => { toggle(e, 0) }}></div>
-                    <div className="boxes" onClick={(e) => { toggle(e, 1) }}></div>
-                    <div className="boxes" onClick={(e) => { toggle(e, 2) }}></div>
+                    <div className="boxes" onClick={(e) => { data[0] === "" && toggle(e, 0) }}>
+                        <img src={data[0] === "x" ? cross_icon : data[0] === 'o' ? circle_icon : ""} />
+
+                    </div>
+                    <div className="boxes" onClick={(e) => { data[1] === "" && toggle(e, 1) }}> <img src={data[1] === "x" ? cross_icon : data[1] === 'o' ? circle_icon : ""} /></div>
+                    <div className="boxes" onClick={(e) => { data[2] === "" && toggle(e, 2) }}> <img src={data[2] === "x" ? cross_icon : data[2] === 'o' ? circle_icon : ""} /></div>
                 </div>
                 <div className="row">
-                    <div className="boxes" onClick={(e) => { toggle(e, 3) }}></div>
-                    <div className="boxes" onClick={(e) => { toggle(e, 4) }}></div>
-                    <div className="boxes" onClick={(e) => { toggle(e, 5) }}></div>
+                    <div className="boxes" onClick={(e) => { data[3] === "" && toggle(e, 3) }}> <img src={data[3] === "x" ? cross_icon : data[3] === 'o' ? circle_icon : ""} /></div>
+                    <div className="boxes" onClick={(e) => { data[4] === "" && toggle(e, 4) }}> <img src={data[4] === "x" ? cross_icon : data[4] === 'o' ? circle_icon : ""} /></div>
+                    <div className="boxes" onClick={(e) => { data[5] === "" && toggle(e, 5) }}> <img src={data[5] === "x" ? cross_icon : data[5] === 'o' ? circle_icon : ""} /></div>
                 </div>
                 <div className="row">
-                    <div className="boxes" onClick={(e) => { toggle(e, 6) }}></div>
-                    <div className="boxes" onClick={(e) => { toggle(e, 7) }}></div>
-                    <div className="boxes" onClick={(e) => { toggle(e, 8) }}></div>
+                    <div className="boxes" onClick={(e) => { data[6] === "" && toggle(e, 6) }}> <img src={data[6] === "x" ? cross_icon : data[6] === 'o' ? circle_icon : ""} /></div>
+                    <div className="boxes" onClick={(e) => { data[7] === "" && toggle(e, 7) }}> <img src={data[7] === "x" ? cross_icon : data[7] === 'o' ? circle_icon : ""} /></div>
+                    <div className="boxes" onClick={(e) => { data[8] === "" && toggle(e, 8) }}> <img src={data[8] === "x" ? cross_icon : data[8] === 'o' ? circle_icon : ""} /></div>
                 </div>
             </div>
         </div>
